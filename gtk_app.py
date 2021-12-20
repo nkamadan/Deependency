ad252280296de30f84673ceee26f18f854b018c1
nkamadan <nkamadan@sabanciuniv.edu>
2021-12-20 17:05:32 +0300

    add multiple columns to the treeview

--- gtk_app.py
+++ gtk_app.py
@@ -3,27 +3,6 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-
-# class Handler:
-#     def onDestroy(self, *args):
-#         Gtk.main_quit()
-
-#     def onButtonPressed(self, button):
-#         print("Hello World!")
-
-# def on_btn1_clicked(self,widget):
-#     print("deniyoruz aga")
-
-# builder = Gtk.Builder()
-# builder.add_from_file("deneme2.glade")
-# #builder.connect_signals(Handler())
-
-# window = builder.get_object("window1")
-
-# window.show_all()
-
-# Gtk.main()
-
 class Main:
     def __init__(self):
         gladeFile = "deneme2.glade"
@@ -32,7 +11,7 @@ class Main:
         self.builder.connect_signals(self)
 
         self.treeview = self.builder.get_object("treeView")
-        self.pathListStore = Gtk.ListStore(str)
+        self.pathListStore = Gtk.ListStore(str,str)
         window = self.builder.get_object("window1")
         
         self.populateTreeView()
@@ -41,14 +20,29 @@ class Main:
         window.show()
 
     def populateTreeView(self):
-        pass1 = ["abc","klm","hello"]
-        for elem in pass1:
-            self.pathListStore.append([elem])
+        pass1 = ["/bin/ls","/bin/cat","/bin/netcat"]
+        pass2 = ["User","root","User"]
+        for elem in range(0,len(pass1)):
+            self.pathListStore.append([pass1[elem],pass2[elem]])
 
-        renderer = Gtk.CellRendererText()
-        pathColumn = Gtk.TreeViewColumn(title="deneme",cell_renderer=renderer,text=0)
-        self.treeview.append_column(pathColumn)
-        self.treeview.set_model(self.pathListStore)
+        treeview_columns = ['Binary/Library', 'Privilege']
+        for col_num, name in enumerate(treeview_columns):
+            # align text in column cells of row (0.0 left, 0.5 center, 1.0 right)
+            rendererText = Gtk.CellRendererText(xalign=0.5, editable=False)
+            column = Gtk.TreeViewColumn(name ,rendererText, text=col_num)
+            self.treeview.set_model(self.pathListStore)
+            # center the column titles in first row
+            column.set_alignment(0.2)
+            # make all the column reorderable, resizable and sortable
+            column.set_sort_column_id(col_num)
+            column.set_reorderable(True)
+            column.set_resizable(True)
+            self.treeview.append_column(column)
+
+        # renderer = Gtk.CellRendererText()
+        # column = Gtk.TreeViewColumn(title="Binary/Library",cell_renderer=renderer,text=0)
+        # self.treeview.append_column(pathColumn)
+        #self.treeview.set_model(self.pathListStore)
 
 
 if __name__ == '__main__':
